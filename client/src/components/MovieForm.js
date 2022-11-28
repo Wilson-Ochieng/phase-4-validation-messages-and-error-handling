be14f107de38cb433bde2631a816eb82a0350b09
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-11-28 05:36:09 +0300

    Completed Assignment

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -14,6 +14,8 @@ function MovieForm() {
     female_director: false,
   });
 
+  const [errors, setErrors] = useState([]);
+
   function handleSubmit(e) {
     e.preventDefault();
     fetch("/movies", {
@@ -23,7 +25,13 @@ function MovieForm() {
       },
       body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((newMovie) => console.log(newMovie));
+        } else {
+          response.json().then((errorData) => setErrors(errorData.errors));
+        }
+      })
       .then((newMovie) => console.log(newMovie));
   }
 
@@ -127,6 +135,13 @@ function MovieForm() {
         </FormGroup>
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
+      {errors.length > 0 && (
+        <ul style={{ color: "red" }}>
+          {errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
     </Wrapper>
   );
 }
